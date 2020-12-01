de8704f0acbc468a4b7d28538e130142c9608192
clanfear <cclanfear@gmail.com>
2020-12-01 11:17:01 -0800

    Initial commit

---
+++ 1/get_2020_product.R
@@ -0,0 +1,17 @@
+input_vec <- read.table("./input.txt")[,1]
+
+get_2020_product_2d <- function(x){
+  ex_mat <- matrix(x, ncol=length(x), nrow=length(x))
+  return((ex_mat * t(ex_mat))[upper.tri(ex_mat) & ((ex_mat + t(ex_mat))==2020)])
+}
+
+get_2020_product(input_vec)
+
+get_2020_product_3d <- function(x){
+  add_array   <- outer(outer(x, x, FUN = "+"), x, FUN = "+")
+  mult_array  <- x %o% x %o% x
+  return(unique(mult_array[add_array==2020])) # Crude
+}
+
+get_2020_product_3d(input_vec)
+
