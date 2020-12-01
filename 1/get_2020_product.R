fef20e967f91cc29691a245312b8b3531a8e378d
clanfear <cclanfear@gmail.com>
2020-12-01 11:23:58 -0800

    Update get_2020_product.R

--- 1/get_2020_product.R
+++ 1/get_2020_product.R
@@ -1,11 +1,15 @@
 input_vec <- read.table("./input.txt")[,1]
 
+# Find product of pair of numbers that sum to 2020
+
 get_2020_product_2d <- function(x){
   ex_mat <- matrix(x, ncol=length(x), nrow=length(x))
   return((ex_mat * t(ex_mat))[upper.tri(ex_mat) & ((ex_mat + t(ex_mat))==2020)])
 }
 
-get_2020_product(input_vec)
+get_2020_product_2d(input_vec)
+
+# Find product of triplet of numbers that sum to 2020
 
 get_2020_product_3d <- function(x){
   add_array   <- outer(outer(x, x, FUN = "+"), x, FUN = "+")
