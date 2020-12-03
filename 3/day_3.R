cd5df70366b374f94715957431a68a678a868127
clanfear <cclanfear@gmail.com>
2020-12-03 11:30:16 -0800

    Update day_3.R

--- 3/day_3.R
+++ 3/day_3.R
@@ -1,6 +1,5 @@
 # Load data
-tree_lines <- 
-  do.call(rbind, strsplit(readLines("./3/input.txt"), ""))
+tree_lines <- do.call(rbind, strsplit(readLines("./3/input.txt"), ""))
 
 # Part 1
 ## Clunky solution but it works
@@ -9,7 +8,7 @@ get_path <- function(trees, x, y){
   width <- ((its*x) %/% ncol(trees))+1
   trees <- matrix(trees, ncol = ncol(trees)*width, nrow=nrow(trees))
   mat   <- cbind(seq(x, x*its, by = x)+1,
-               seq(y, y*its, by = y)+1)
+                 seq(y, y*its, by = y)+1)
   path  <- apply(mat, 1, function(mat) `[`(trees, mat[2], mat[1]))
   return(sum(path == "#"))
 }
